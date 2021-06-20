c7cbda99fe06d0528880684a2919ff47ab79c9d2
YoussifAssem <youssifassem1815221@miuegypt.edu.eg>
2021-06-21 01:44:33 +0200

    Update Student Affairs
    Add Student Remove Student FrontEnd && BackEnd

---
+++ app/controller/removeStudentController.php
@@ -0,0 +1,19 @@
+<?php
+require_once "Controller.php";
+
+class removeStudentController extends Controller{
+    public function removeStudent(){
+        if(isset($_POST['sub'])){
+            if(isset($_POST['delete'])){
+              foreach($_POST['delete'] as $dele){
+                 $this->model->removeStudent($dele);
+              }
+           }  
+              else{
+                 echo "ERROR U SHOULD SELECT USER TO DELETE IT";
+                }
+                header("refresh: 0.1");  
+          }
+    }
+}
+?>
