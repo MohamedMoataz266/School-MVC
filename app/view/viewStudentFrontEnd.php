b7b85b9718126cf6b22d54efc10a8924e9df1439
kareem644 <76753975+kareem644@users.noreply.github.com>
2021-06-22 23:39:47 +0200

    update

--- app/view/viewStudentFrontEnd.php
+++ app/view/viewStudentFrontEnd.php
@@ -7,18 +7,22 @@ class viewStudentFrontEnd{
     
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src='<?php echo URLROOT; ?>public/js/menu.js'></script>
-<style>
-	img{
-		width: 60%;
-		height: 60%;
-	}
-</style>
+<link rel="stylesheet" href="<?php echo URLROOT; ?>public/css/studentfrontend.css">;
 <?php require APPROOT.'/model/homemenu.php';?>
 <body onload='studentMenu()'>
+<div class="register">
+<?php
+session_start();
+?>
+<h1>Welcome </h1>
+<h2>Student</h2>
+<strong><?php echo $_SESSION['name']; ?></strong>
+</div>
+
 </body>
-
-
-<?php 
+<?php
 }
 }
 ?>
+
+	
