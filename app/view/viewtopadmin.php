8b337c88d1190295a7450588d205606704d464b1
kareem644 <76753975+kareem644@users.noreply.github.com>
2021-06-22 23:03:28 +0200

    topadmin

---
+++ app/view/viewtopadmin.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+class viewtopadmin{
+    public function output(){
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src='<?php echo URLROOT; ?>public/js/menu.js'></script>
+
+<link href="<?php echo URLROOT; ?>public/css/topadmin.css" rel="stylesheet" media="all">
+<div class="image">
+<img src="Images/amoun.jpeg" alt="">
+</div>
+<div class="container">
+<nav>
+  <ul>
+    <li><button onclick="window.location.href='teacherFrontEnd.php'">
+      Teacher Side
+      <span></span><span></span><span></span><span></span>
+    </button></li>
+    <li><button onclick="window.location.href='studentAffairesFrontEnd.php'">
+      Student Affairs Side
+      <span></span><span></span><span></span><span></span>
+    </button></li>
+    <li><button onclick="window.location.href='personal.php'">
+      Personnal Side
+      <span></span><span></span><span></span><span></span>
+    </button></li>
+    
+  </ul>
+</nav>
+</div>
+<?php 
+}
+}
+?>
---
+++ public/topAdmins.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../app/db/config.php';
+require_once '../app/view/viewtopadmin.php';
+
+
+
+$view = new viewtopadmin();
+$view->output();
+
+?>
