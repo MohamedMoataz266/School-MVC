da4298f3910c67d2c5b0c12f253b575e0cf30e2b
YoussifAssem <youssifassem1815221@miuegypt.edu.eg>
2021-06-24 00:55:54 +0200

    Done with Home Page
    Home Page errors

--- app/model/homePage.php
+++ app/model/homePage.php
@@ -13,48 +13,45 @@ class homePage extends Model{
 public function make_slide_indicators(){
 parent::connect();    
  $count = 0;
+ $output = ''; 
  $result = $this->make_query();
  while($row = mysqli_fetch_array($result)){
   if($count == 0){
-      ?>
-   <li data-target="#dynamic_slide_show" data-slide-to="'.$count.'" class="active"></li>
-  <?php
+    $output .= '<li data-target="#dynamic_slide_show" data-slide-to="'.$count.'" class="active"></li>';
   }
   else{
-      ?>
-   <li data-target="#dynamic_slide_show" data-slide-to="'.$count.'">
-   <?php
-  }
+    $output .= '
+    <li data-target="#dynamic_slide_show" data-slide-to="'.$count.'"></li>
+    ';
+  } 
   $count = $count + 1;
   }
+  return $output;
 }
 
 public function make_slides(){
 parent::connect();    
  $count = 0;
+ $output = '';
  $result = $this->make_query();
 
  while($row = mysqli_fetch_array($result)){
   if($count == 0){
-      ?>
-   <div class="item active">
-  <?php
+  $output .= '<div class="item active">';
   }
   else{
-      ?>
-    <div class="item">
-    <img src='<?php echo URLROOT; ?>public/images/<?php echo $row['banner_image']; ?>' alt='<?php echo $row['banner_image']; ?>'>
-    <div class="carousel-caption">
+    $output .= '<div class="item">';
+  }
+  $output .= '
+  <img src="images/'.$row["banner_image"].'" alt="'.$row["banner_title"].'" />
+  <div class="carousel-caption">
      <h3><?php echo $row["banner_title"] ?></h3>
     </div>
-   </div>
-     <?php
-}
+   </div>';
   
   $count = $count + 1;
   }
+return $output;
  }
 }
-
-
 ?>
--- app/view/viewHomePage.php
+++ app/view/viewHomePage.php
@@ -6,20 +6,16 @@ class viewHomePage extends View{
       $home = new homePage();
    ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src='<?php echo URLROOT; ?>public/js/menu.js'></script>
-<?php require APPROOT.'/model/homemenu.php';?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="<?php echo URLROOT; ?>public/css/slideshow.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   
+<link rel="stylesheet" href="<?php echo URLROOT; ?>public/css/slideshow.css">
+<script src='<?php echo URLROOT; ?>public/js/menu.js'></script>
+<?php require APPROOT.'/model/homemenu.php';?>
   
   
 </head>
@@ -60,6 +56,7 @@ class viewHomePage extends View{
 <?php require APPROOT.'/model/viewfooter.php';?>
 
 </body>
+</html>
    <?php
       
     }
